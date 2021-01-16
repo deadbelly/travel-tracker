5e656ff998f350a85507efa951a4e9fbb877ac3e
Boone Epstein <booneepstein@gmail.com>
2021-01-15 23:23:04 -0600

    Improve trip list layout

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -12,6 +12,14 @@ const domUpdates = {
         <img src="${destination.image}"  alt="${destination.alt}">
         <div class="info-block">
           <h2>${destination.destination}</h2>
+          <p class="year">${parseInt(trip.date)}</p>
+          <h3>${trip.isPending()}</h3>
+          <div class="details">
+            <p>start date:  ${trip.date}</p>
+            <p>duration:  ${trip.duration} days</p>
+            <p>travelers:   ${trip.travelers}</p>
+            <p>cost:  $${trip.calculateCost(destinations)}</p>
+          </div>
         <div>
       </button>
     </article>`);
